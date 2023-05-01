30be596e9e706b39393834f70da8461aae61ac6c
Sergio Candalija Valderrama <sergiocava.d@gmail.com>
2023-05-01 21:54:28 +0200

    Add Dockerfile and DockerCompose

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import svelte from "@astrojs/svelte";
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), svelte()],
   output: "server",
   adapter: deno()
 });
