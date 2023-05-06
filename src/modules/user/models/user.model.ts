3d1c9f5a4c6400bb8d821d10feea27d0ea85a275
Germangalia <ggalia84@gmail.com>
2023-05-06 11:08:18 +0200

    Improve user model with token

--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -3,7 +3,7 @@ import { mongoose } from "../../../../deps.ts";
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  confirmed: { type: Date },
+  token: { type: String }
 });
 
 export default mongoose.model('User', userSchema);
