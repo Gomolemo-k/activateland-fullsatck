0496c4f681d80e9115dc500be42784eb4cd5dfbe
Germangalia <ggalia84@gmail.com>
2023-05-22 16:19:36 +0200

    Add dashboard modules with React.

---
+++ src/modules/dashboard/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
+
+const Index = () => {
+  return (
+    <div className='overflow-hidden'>
+     <ContextProvider>
+       <App/>
+     </ContextProvider>
+    </div>
+  );
+};
+
+export default Index;
