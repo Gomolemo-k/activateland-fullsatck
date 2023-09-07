cdfa58dae6dfb83205ba9de934697dabb8cc9eac
German Galia Beltran <ggalia84@gmail.com>
2023-09-07 18:33:45 +0200

    Try to push git test

--- src/assets/data/dashboard-routes/data.jsx
+++ src/assets/data/dashboard-routes/data.jsx
@@ -16,7 +16,7 @@ import avatar4 from '../../img/dashboard-routes/avatar4.jpg';
 
 export const stackedChartData = [
   [
-    { x: 'Jan', y: 111.1 },
+    { x: 'Jan', y: 111.2 },
     { x: 'Feb', y: 127.3 },
     { x: 'Mar', y: 143.4 },
     { x: 'Apr', y: 159.9 },
