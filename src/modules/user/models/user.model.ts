55c1bc1d50018c3d9513830a85b665f145d449bc
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-13 17:51:14 +0200

    changed user model using prisma format and crude method

--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -1,59 +1,75 @@
-import { mongoose}  from "../../../../deps.ts";
-import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
-import jwt from "npm:jsonwebtoken@9.0.0";
-import Joi from "npm:joi@17.9.2";
+import { PrismaClient } from '@prisma/client';
+import Joi from 'joi';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  workspaces: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workspace' }],
-  userProfile: { type: mongoose.Schema.Types.ObjectId, ref: 'UserProfile' },
-  userAccount: { type: mongoose.Schema.Types.ObjectId, ref: 'UserAccount' },
-  userSession: { type: mongoose.Schema.Types.ObjectId, ref: 'UserSession' },
-}, {
-	timestamps: true
-});
+const prisma = new PrismaClient();
 
-function generateRandomPassword(): string {
-	const randomString = Math.random().toString(36).slice(-8);
-	return randomString;
+type UserCreateBody = PrismaClient.Args<typeof prisma.user, 'create'>['data'];
+type UserUpdateBody = PrismaClient.Args<typeof prisma.user, 'update'>['data'];
+
+export async function listUsers() { 
+    return await prisma.user.findMany();
+}
+
+export async function updateOrCreateUser(body: UserCreateBody | UserUpdateBody) {
+    const user = await prisma.user.upsert({
+        where: { email: body.email },
+        update: { data: body },
+        create: { data: body }
+    });
+    return user;
+}
+
+export async function deleteUser(id: string) {
+    return await prisma.user.delete({
+        where: { id: parseInt(id) }
+    });
+}
+
+export async function getUserReferences(userId: string) {
+    return await prisma.user.findUnique({
+        where: { id: parseInt(userId) },
+        include: {
+            userAccount: true,
+            userProfile: true,
+            userSession: true,
+            Workspaces: {
+                include: {
+                    teams: {
+                        include: {
+                            teamMembers: true
+                        }
+                    }
+                }
+            },
+            workspaces: {
+                include: {
+                    properties: {
+                        include: {
+                            propertyAnalysis: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+export const validate = (data: { email: string, password: string }) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required().label("Email"),
+        password: Joi.string().required().label("Password"),
+    });
+    return schema.validate(data);
+}
+
+export function generateRandomPassword(): string {
+    const randomString = Math.random().toString(36).slice(-8);
+    return randomString;
 }
 
 async function hashPassword(pass: string): Promise<string> {
-	const salt = await bcrypt.genSalt(Number(Deno.env.get('SALT')||'8'));
-	const hashPass = await bcrypt.hash(pass, salt);
-	return hashPass;
+    const salt = await bcrypt.genSalt(Number(Deno.env.get('SALT') || '8'));
+    const hashPass = await bcrypt.hash(pass, salt);
+    return hashPass;
 }
-
-userSchema.pre('validate', function(next) {
-	if (!this.password) {
-	  this.password = generateRandomPassword();
-	}
-	next();
-  });
-
-userSchema.pre("save", async function(next) {
-	if (!this.isModified("password")) return next();
-	try {
-		if (!this.password) this.password = generateRandomPassword();
-		if (this.password) this.password = await hashPassword(this.password);
-		return next();
-	} catch (error) {
-		console.log(error)
-	  	return next(error);
-	}
-});
-
-const validate = (data: { email: string, password: string }) => {
-	const schema = Joi.object({
-		email: Joi.string().email().required().label("Email"),
-		password: Joi.string().required().label("Password"),
-	});
-	return schema.validate(data);
-};
-
-// export default mongoose.model("User", userSchema);
-
-const User = mongoose.model("User", userSchema);
-
-export { User, validate, generateRandomPassword, hashPassword };
