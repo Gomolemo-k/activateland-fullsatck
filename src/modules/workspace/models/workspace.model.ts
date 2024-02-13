c67514433e3d9c68667c3f077c295784a24f4522
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-13 13:51:52 +0200

    changed workspace model using prsima format and crude method

--- src/modules/workspace/models/workspace.model.ts
+++ src/modules/workspace/models/workspace.model.ts
@@ -1,14 +1,37 @@
-import { mongoose}  from "../../../../deps.ts";
+import { PrismaClient } from '@prisma/client';
 
-const workspaceSchema = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	name: { type: String, required: true },
-	description: { type: String },
-	teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
-	properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }],
-}, {
-	timestamps: true
-});
+const prisma = new PrismaClient();
+
+type WorkspaceCreateBody = PrismaClient.Args<typeof prisma.workspace, 'create'>['data'];
+type WorkspaceUpdateBody = PrismaClient.Args<typeof prisma.workspace, 'update'>['data'];
+
+export async function listWorkspaces() {
+  return await prisma.workspace.findMany();
+}
+
+export async function getWorkspaceById(id: string) {
+  return await prisma.workspace.findUnique({
+    where: { id: id }
+  });
+}
+
+export async function createWorkspace(body: WorkspaceCreateBody) {
+  return await prisma.workspace.create({
+    data: body
+  });
+}
+
+export async function updateWorkspace(id: string, body: WorkspaceUpdateBody) {
+  return await prisma.workspace.update({
+    where: { id: id },
+    data: body,
+  });
+}
+
+ export async function deleteWorkspace(id: string) {
+  return await prisma.workspace.delete({
+    where: { id: id }
+  });
+}
 
 
-export default mongoose.model("Workspace", workspaceSchema);
