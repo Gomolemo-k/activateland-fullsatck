9691e16f7480195d5a8c38382c9557775bf5c93e
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-13 13:55:17 +0200

    changed user session model using prsima format and crude method

--- src/modules/user-session/models/user-session.model.ts
+++ src/modules/user-session/models/user-session.model.ts
@@ -1,12 +1,34 @@
-import { mongoose}  from "../../../../deps.ts";
+import { PrismaClient } from '@prisma/client';
+import prisma from "../../../../client.ts";
 
-const userSessionSchema  = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	token: { type: String, unique: true, required: true },
-	expiresAt: { type: Date },  
-}, {
-	timestamps: true
-});
+type UserSessionCreateBody = PrismaClient.Args<typeof prisma.userSession, 'create'>['data'];
+type UserSessionUpdateBody = PrismaClient.Args<typeof prisma.userSession, 'update'>['data'];
 
+export async function listUserSessions() {
+    return await prisma.userSession.findMany();
+}
 
-export default mongoose.model("UserSession", userSessionSchema);
+export async function createUserSession(body: UserSessionCreateBody) {
+    return await prisma.userSession.create({ 
+		data: body
+	});
+}
+
+export async function getUserSessionById(id: string) {
+    return await prisma.userSession.findUnique({ 
+		where: { id: id } 
+	});
+}
+
+export async function updateUserSession(id: string, body: UserSessionUpdateBody) {
+    return await prisma.userSession.update({ 
+		where: { id:id }, 
+		data: body
+	});
+}
+
+export async function deleteUserSession(id: string) {
+    return await prisma.userSession.delete({ 
+		where: { id: id } 
+	});
+}
