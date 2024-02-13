64e39301d6dfe4cf8ae344b5a090c2c4404a5bd0
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-13 14:11:03 +0200

    changed user profile model using prisma format and crude method

--- src/modules/user-profile/models/user-profile.model.ts
+++ src/modules/user-profile/models/user-profile.model.ts
@@ -1,15 +1,35 @@
-import { mongoose}  from "../../../../deps.ts";
+import { PrismaClient } from '@prisma/client';
 
-const userProfileSchema  = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	bio: { type: String },
-	userPhoto: { type: mongoose.Schema.Types.ObjectId, ref: 'UploadedFile' },
-	userLocation: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
-}, {
-	timestamps: true
-});
+const prisma = new PrismaClient();
 
+type UserProfileCreateBody = PrismaClient.Args<typeof prisma.userprofile, 'create'>['data'];
+type UserProfileUpdateBody = PrismaClient.Args<typeof prisma.userprofile, 'update'>['data'];
 
-export default mongoose.model("UserProfile", userProfileSchema);
+export async function listUserProfiles () {
+  return await prisma.user.findMany();
+}
+
+export async function getUserProfileById(id: string) {
+  return await prisma.user.findUnique({
+    where: { id: id }
+  });
+}
+
+export async function createUserProfile(data: UserProfileCreateBody) {
+  return await prisma.user.create({
+    data: data
+  });
+}
+
+export async function updateUserProfile(id: string, data: UserProfileUpdateBody) {
+  return await prisma.user.update({
+    where: { id: id },
+    data: data
+  });
+}
+
+export async function deleteUserProfile(id: string) {
+  return await prisma.user.delete({
+    where: { id: id }
+  });
+}
