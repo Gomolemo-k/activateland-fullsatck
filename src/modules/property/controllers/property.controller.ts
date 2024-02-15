9021284d39861104e16dc9f15c645cd9fe8e42ea
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-15 20:33:47 +0200

    corrected the spelling mistake and format

--- src/modules/property/controllers/property.controller.ts
+++ src/modules/property/controllers/property.controller.ts
@@ -1,9 +1,8 @@
-import {listProperties, getPropertyById, createProperty, updateProperty, deleteProperty, propertyReferences  } from "../models/property.model.ts";
+import {listProperties, getPropertiesByWorkspaceId, getPropertyById,  getPropertyReferences, createProperty, updateProperty, deleteProperty} from "../models/property.model.ts";
 import { RouterContext } from "https://deno.land/x/oak@v12.4.0/mod.ts";
-import { Property, propertyType } from 
 
 const notFoundMessage = "Record not found in database.";
-const iternalServerErrorMessage = "Internal Server Error.";
+const internalServerErrorMessage = "Internal Server Error.";
 const createdMessage = "Record created successfully.";
 const deletedMessage = "Record deleted successfully.";
 
@@ -13,18 +12,17 @@ export const list = async (ctx: RouterContext<any, any>) => {
         ctx.response.body = record;
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
 
-export const listByWorkpsace = async (ctx: RouterContext<any, any>) => {
+export const listByWorkspace = async (ctx: RouterContext<any, any>) => {
     try {
-        const record = await Property.find({workspace: ctx.params?.workspaceId});
-        // console.log('record: ', record)
+        const record = await getPropertiesByWorkspaceId(ctx.params?.workspaceId)
         ctx.response.body = record;
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
 
@@ -38,28 +36,41 @@ export const get = async (ctx: RouterContext<any, any>) => {
         ctx.response.body = record;
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
+    }
+};
+
+export const getReferences = async (ctx: RouterContext<any, any>) => {
+    try {
+        const record = await getPropertyReferences(ctx.params?.id);
+        if (!record) {
+            ctx.response.status = 404;
+            ctx.response.body = { message: notFoundMessage };
+        }
+        ctx.response.body = record;
+    } catch (error) {
+        ctx.response.status = 500;
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
 
 export const create = async (ctx: RouterContext<any, any>) => {
     try {
-        const reqBody = await createProperty().value;
-        const record = new Property(reqBody);
+        const reqBody = await ctx.request.body().value;
+        const record = createProperty(reqBody);
         await record.save();
-
         ctx.response.status = 201;
         ctx.response.body = record;
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
 
 export const update = async (ctx: RouterContext<any, any>) => {
     try {
         const reqBody = await ctx.request.body().value;
-        const record = await Property.updateProperty(ctx.params?.id, reqBody, {new: true});
+        const record = await updateProperty(ctx.params?.id, reqBody);
         if (!record) {
             ctx.response.status = 404;
             ctx.response.body = { message: notFoundMessage };
@@ -67,7 +78,7 @@ export const update = async (ctx: RouterContext<any, any>) => {
         return ctx.response.body = record;
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
 
@@ -81,23 +92,6 @@ export const destroy = async (ctx: RouterContext<any, any>) => {
         ctx.response.body = { message: deletedMessage };
     } catch (error) {
         ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
-    }
-};
-
-export const getPropertyReferences = async (ctx: RouterContext<any, any>) => {
-    try {
-        const property = await PropertyReferences({_id: ctx.params?.id})
-        .populate(
-            {
-                path: 'propertyAnalysis',
-                model: 'PropertyAnalysis'
-            });
-      
-        // console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%getUserReferences: ', user)
-        ctx.response.body = property;
-    } catch (error) {
-        ctx.response.status = 500;
-        ctx.response.body = { message: iternalServerErrorMessage, error: error };
+        ctx.response.body = { message: internalServerErrorMessage, error: error };
     }
 };
