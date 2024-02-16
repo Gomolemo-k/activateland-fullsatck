bc0f8979298e2bb26b32a00ecba1f833c071d8db
Gomolemo-k <kgatitsoeg321@gmail.com>
2024-02-16 16:39:00 +0200

    deleted an import and function

--- src/modules/user-profile/controllers/user-profile.controller.ts
+++ src/modules/user-profile/controllers/user-profile.controller.ts
@@ -1,10 +1,8 @@
 import {listUserProfiles, getUserProfileById, createUserProfile,updateUserProfile, deleteUserProfile } from "../models/user-profile.model.ts";
-import UserProfile from "../models/user-profile.model.ts";
 import { RouterContext } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
 const notFoundMessage = "Record not found in database.";
 const internalServerErrorMessage = "Internal Server Error.";
-const createdMessage = "Record created successfully.";
 const deletedMessage = "Record deleted successfully.";
 
 export const list = async (ctx: RouterContext<any, any>) => {
@@ -35,8 +33,6 @@ export const create = async (ctx: RouterContext<any, any>) => {
     try {
         const reqBody = await ctx.request.body().value;
         const record = createUserProfile(reqBody);
-        await record.save();
-
         ctx.response.status = 201;
         ctx.response.body = record;
     } catch (error) {
